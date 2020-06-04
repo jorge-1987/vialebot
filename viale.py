80e41b66ebd112944e661d7e8da82866e6f1627c
Jorge Abreu <jorgeabreu87@gmail.com>
2020-06-03 20:08:13 -0500

    Starting the proyect

---
+++ viale.py
@@ -0,0 +1,8 @@
+import twitter
+
+
+
+api = twitter.Api(consumer_key=[consumer key],
+                  consumer_secret=[consumer secret],
+                  access_token_key=[access token],
+                  access_token_secret=[access token secret])
