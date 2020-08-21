a633a259aae059bd242969c3b395773d8801c9b6
Jorge Abreu <jorgeabreu87@gmail.com>
2020-08-20 20:19:13 -0500

    cambio a famosos

--- viale.py
+++ viale.py
@@ -4,17 +4,86 @@ import random
 
 api = twitter.Api(consumer_key='key',
                   consumer_secret='secret',
-                  access_token_key='token key',
-                  access_token_secret='tokensecret')
+                  access_token_key='tkey',
+                  access_token_secret='tsecret')
 
-pelea = ["VIALE: Usted viene a defender 'el compra' argentino, 'ta muy bien.","SAMID: Yo estoy defendien--","VIALE: Esto es lo que--Esto es lo que defiende el mundo entero.","SAMID: La competividad que estoy defendiendo; es que si nosotro no tenemo una moneda más fuerte--","VIALE: Si","SAMID: Una moneda que no sea tan fuerte--","VIALE: Si","SAMID: Aca se van a, volver a abrir las fábricas.","VIALE: Usted tiene un razonamiento primario-","SAMID: Si supiera cómo se llama usted--","VIALE: Me cuesta entenderlo, porque usted tiene un razonamiento muy primario, es--","SAMID: Si, 'ta bien.","VIALE: Es muy elemental.","SAMID: Primero averigüe como se llama; Yo a la mañana me levanto y se que soy Alberto Samid--","VIALE: No sabe de compra--","SAMID: Usted no sabe si es Viale, José, Pérez o Rodriguez. Qué va a saber!","VIALE: José seguro que no lo soy.","SAMID: Pero bueno.","VIALE: Alberto, pague-- pague los impuestos, déjese de joder--","SAMID: Ya los pagué--","VIALE: No, pague los impuestos. Usted debe como 30 millones de dólares--","SAMID: Y usted, y usted a ver si sabe cómo se llama.","VIALE: Pero pague los impuestos, pero yo pago los impuestos.--","SAMID: A ver si sabe como se llama un dia.","VIALE: Usted debe como 70 millones, pague los impuestos, eh.","SAMID: Ojalá algun día sepa como se llama.","VIALE: A ver, una cosita más...","SAMID: No, deje, deje.","VIALE: Por favor--","SAMID: Cómo es su nombre?","VIALE: Una última pregunta mas.","SAMID: Dígame cómo se llama","VIALE: Una última pregunta mas.","SAMID: Si usted me dice como se llama, yo le digo la pregunta que quiera.","VIALE: Por favor.","SAMID: Cómo se llama usted?","VIALE: Ya pasó, Samid--","SAMID: No, pero no sé a quien le voy a decir--","VIALE: Me va a contestar la última?","SAMID: A quién le voy a contestar? Si no sé cómo se llama usted.","VIALE: Me va--","SAMID: Dígame como se llama.","VIALE: Me va a contestar?","SAMID: Si usted me dice como se llama, si.","VIALE: Alberto, ya terminó--","SAMID: Cómo se llama usted?","VIALE: Alberto yo conozco la histeria, no me haga histeria--","SAMID: No le hago histeria.","VIALE: Puro barullo es esto--","SAMID: Chau, 'ta luego.","VIALE: No se anima a contestarme la última?","SAMID: Si, yo me animo a contestarle donde quiera.","VIALE: Seguro?","SAMID: Si, seguro.","VIALE: Uste' avaló--","SAMID: No descalifique.","VIALE: Uste' avaló la bomba a la AMIA.","SAMID: No, no puede decir semejante barbaridad, Mire, la verdad--","VIALE: Usted avaló la bomba.","SAMID: La verdad. Mire, la verdad-","VIALE: Uste' avaló la bomba a la AMIA.","SAMID: De la única persona en el mundo, que yo puedo esperar que diga semejante barbaridad, es de usted. Es de usted- Usted no puede decir semejante barbaridad--","VIALE: Vio?","SAMID: Usted no puede decir semejante barabari--","VIALE: Nos conocemos bien eh?","SAMID: No, no puede decir eso.","VIALE: Vio? Nos conocemos bien.","SAMID: Usted tiene que arrepentirse de lo que dijo.","VIALE: No, no me voy a arrepentir.","SAMID: Usted se tiene que arrepentir, porque yo no hice nada de eso.","VIALE: NO, no. Bueno, tranquilo--","SAMID: Usted no puede decir--No puede decir eso.","Golpe de Samid a Viale","VIALE: No, eso no!","SAMID: Cómo vas a decir eso? JUDIO HIJO DE @*74!","VIALE: Pará, pará.","Patada de Viale de 6to grado, más pelea.","SAMID: Te va' mataá! Cómo vas a decir que yo--"]
+lista = ["Usted viene a defender 'el compra' argentino, 'ta muy bien. - Mauro Viale",
+"Usted tiene un razonamiento primario. - Mauro Viale",
+"Si, 'ta bien. - Alberto Samid",
+"¿Un plato típico de Rusia?... la ensalada rusa - Wanda Nara",
+"Es muy elemental. - Mauro Viale",
+"Alberto, pague-- pague los impuestos, déjese de joder-- - Mauro Viale",
+"Una última pregunta mas. - Mauro Viale",
+"¿Cuántos pulmones tengo? Uno, como todo el mundo - Mostaza Merlo",
+"Me va a contestar la última? - Mauro Viale",
+"Alberto yo conozco la histeria, no me haga histeria-- - Mauro Viale",
+"No le hago histeria. - Alberto Samid",
+"Mi voto no es positivo - Julio Cobos",
+"Puro barullo es esto-- - Mauro Viale",
+"Chau, 'ta luego. - Alberto Samid",
+"Uste' avaló la bomba a la AMIA.",
+"No, no puede decir semejante barbaridad, Mire, la verdad--",
+"Usted avaló la bomba. - Mauro Viale",
+"SLa verdad. Mire, la verdad- - Alberto Samid",
+"Uste' avaló la bomba a la AMIA. - Mauro Viale",
+"De la única persona en el mundo, que yo puedo esperar que diga semejante barbaridad, es de usted. Es de usted- - Alberto Samid",
+"Usted no puede decir semejante barabaridad--  - Alberto Samid",
+"El 2001 será un gran año para todos. ¡Qué lindo es dar buenas noticias! - Fernando De la Rúa",
+"Nos conocemos bien eh? - Mauro Viale",
+"No, no puede decir eso. - Alberto Samid",
+"Usted tiene que arrepentirse de lo que dijo. - Alberto Samid",
+"No, no me voy a arrepentir. - Mauro Viale",
+"NO, no. Bueno, tranquilo-- - Mauro Viale",
+"Usted no puede decir--No puede decir eso. - Alberto Samid",
+"Rock es AC/DC. ¿Fito Páez se parece a AC/DC? Entonces no es rock. - Pappo",
+"Golpe de Samid a Viale - Alberto Samid",
+"Patada de Viale de 6to grado, más pelea.  - Mauro Viale",
+"Te va' mataá! Cómo vas a decir que yo-- - Alberto Samid",
+"Si yo fuera almacenero, mis canciones hablarian de salames, de mortadela, de quesos. - Pappo",
+"Yo no manejo el rating, yo manejo un Rolls-Royce - Ricardo Fort",
+"Sale plata, sale plata, sale plata",
+"Basta, chicos - Ricardo Fort",
+"Miameeeeeeeeeee - Ricardo Fort",
+"YENDO O LLENDO... SE IGUAL PORQUE VOY ARRIBA DE MI ROLLS ROYCE...  ASI QUE A YOORAR O LLORARR AL CAMPITO... JJAJAJAJA - Ricardo Fort",
+"LA GUITA ESTA PARA GASTARLA Y CON TODOS MIS QUILOMBOS DE SALUD ESTO ES UN ALICIENTE PARA MIS DOLORES QUE CARAJO!!! - Ricardo Fort",
+"ESTA ES LA GENTE QUE QUIERO VER EN LA TELEVISIÓN ... - Ricardo Fort",
+"Yo no soy una persona importante. Soy importante como persona, que es distinto. - Pappo",
+"POR FAVOR PAÍS RECEN POR @CFKArgentina PARA QUE HOY SALGA TODO BIEN EN SU OPERACIÓN . UNÁMONOS UNA VEZ . VAMOS PAÍS!!!!! VAMOS CRISTINA!!!!! - Ricardo Fort",
+"BASTA DE SUBESTIMAR AL PÚBLICO . LLAMEN ACTORES!!!! - Ricardo Fort",
+"Yo me cago de risa de todo. No me interesa que la gente lea lo que hablo. No tengo nada que decir... - Pappo",
+"UNA VERGÜENZA Y UN INSULTO AL TELEVIDENTE. NO SE SOPORTA VERLO Y OÍR LOS DIÁLOGOS PAUPERRIMOS FALTOS DE ACTORES. AMPLIARE EN MI COLUNA. - Ricardo Fort",
+"ALGUIEN LE PUEDE DECIR EL PAPEL LAMENTABLE QUE ESTA HACIENDO?? - Ricardo Fort",
+"No cambia nada estar un poco sucio si mi cabeza es eficaz. - Pappo",
+"GANO QUIEN TENIA QUE GANAR... - Ricardo Fort",
+"Son muchos pensamientos para una sola cosa, estoy algo cansado de vivir en realidad. - Pappo",
+"HOY ROJO CONTRA LA ENVIDIA... - Ricardo Fort",
+"VIDA NUEVA.PARA ADELANTE SIEMPRE. PARA ATRÁS NI PARA TOMAR ENVIÓN. EL PASADO YA NO EXISTE. IT'S OVER!!! ATRÁS QUEDAN LOS PERDEDORES. YO SIGO - Ricardo Fort",
+"Cuando pienso que algo es injusto y me siento un poco solo, el blues me sale mejor. - Pappo",
+"NO TIENE SENTIDO AMAR A ALGUIEN QUE NO TE AMA Y NO SE LA JUEGA POR UNO. LO MEJOR ES SEGUIR ADELANTE Y DEJAR ATRÁS LO QUE TE LASTIMA... - Ricardo Fort",
+"PARECE QUE ES LA HORA QUE SALEN TODOS LOS PELOTUDOS A ESCRIBIR HUEVADAS EN TWITTER... ME VOY AL GYM.. - Ricardo Fort",
+"¿Qué quiero hacer? Cambiar el mundo. ¿Me creo Jesucristo? Puede ser. ¿Quiero un mundo mejor? sí - Fernando Peña",
+"os un payaso mediático - Alejandro Fabri",
+"Casi la totalidad de los músicos de rock argentino son unos pajeros. Yo no los invitaría a comer a mi casa. Bah! ..., a Pipo Cipolatti sí, porque es divertido... - Luca Prodan",
+"En la Argentina hay que dejar de robar dos años - Luis Barrionuevo",
+"Yo soy rubia de adentro y de afuera - Mirtha Legrand",
+"Me acostumbré al fracaso - Pipo Cipolatti",
+"Billetera mata galán - Jacobo Winograd",
+"En las sesiones largas algunos legisladores se van al baño y se dan unos nariguetazos - Alberto Lestelle, secretario para la Lucha contra la Drogadicción, 1995.",
+"Lo dejo a tu criterio - Karina Jelinek",
+"¡Se acabó, de ahora en más voy a cumplir 69! - Susana Giménez",
+"Manchita de tuco - Moria Casan",
+"Éramos tan pobres - Alberto Olmedo",
+"Esas naves espaciales van a salir de la atmósfera y se van a remontar a la estratosfera. De esa manera en una hora y media vamos a poder estar desde la Argentina en Japón o en cualquier otro lugar del mundo - Carlos Menem",
+"Yo nunca he fumado marihuana porque eso da celulitis - Valeria Mazza.",
+"Un dinosaurio… ¿vivo? - Susana Giménez",
+"Si tenés dignidad, echate - Chiche Gelblung",
+"En mi cabeza tengo cuatro botoncitos. El botón de no escuchar, el de callarse la boca, el de desaparecer y el botón de ataque. Con eso resuelvo todas las situaciones. - Pappo",
+"Buscate un trabajo honesto. - Pappo"
+]
 
-#print(len(pelea))
-
-index = random.randint(0, 77)
-twit = str(index) + ': ' + pelea[index]
+index = random.randint(0, (len(lista)-1))
+twit = lista[index]
 #print(twit)
-
 
 status = api.PostUpdate(twit)
 print(status.text)
